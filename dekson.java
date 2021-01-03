bfffd52d82cedf33266a95603ab858fd0fa2eb34
Samriddhi Thapa <samriddhi.thapa3@gmail.com>
2021-01-03 17:24:32 -0600

    dispon man

---
+++ dekson.java
@@ -0,0 +1,15 @@
+package dipsonkale;
+
+public class dekson {
+	
+
+		
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String name= "dipson baaun";
+		 System.out.printf(name);
+
+	}
+
+}
